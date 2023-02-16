0f77255368911515bc7f326934429a99d0c4f446
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-02-16 01:23:50 -0500

    class new commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Classes
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using System;
+
+
+
+
+classes.Car Acar = new classes.Car();
+
+Acar.Make = "Ford";
+Acar.Model = "F-20";
+Acar.Year = 2013;
+
+Console.WriteLine($"{Acar.Make}");
+Console.WriteLine($"{Acar.Model}");
+Console.WriteLine($"{Acar.Year}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
